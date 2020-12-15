7086e706c8db13456efcea169c0e6568f2f92b92
Aiskorim <luisayang9@gmail.com>
2020-12-15 15:44:00 +0100

    Improved test code with more detailed invalid inputs tests

--- tests/test_volley.py
+++ tests/test_volley.py
@@ -5,92 +5,48 @@ import os
 
 # add parent folder to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Main import my_input
+from Main import my_input, info_player, info_team, my_quit
 
-'''
-class TestAnswer(unittest.TestCase):
-    def testA(self):
-        original_raw_input = __builtins__.input
-        __builtins__.input = lambda _: 'A'
-        self.assertEqual(my_input(), 'You chose to have more info about the volleyball player.')
-        __builtins__.input = original_raw_input
-
-    def testB(self):
-        original_raw_input = __builtins__.input
-        __builtins__.input = lambda _: 'B'
-        self.assertEqual(my_input(), 'You chose to have more info about the volleyball team.')
-        __builtins__.input = original_raw_input
-
-'''
-
-
-
+#Test the inputs code
 class TestMain(unittest.TestCase):
 
 
     # smoke test: valid inputs
     def test_correct_A(self):
         # select some valid inputs, for which the output is known
-        self.assertEqual(my_input("A"), "You chose to have more info about the volleyball player.")
+        self.assertEqual(info_player(), print("You chose to have more info about the volleyball player.\n"))
         
         # smoke test: valid inputs
     def test_correct_B(self):
         # select some valid inputs, for which the output is known
-        self.assertEqual(my_input("B"), "You chose to have more info about the volleyball team.")
+        self.assertEqual(info_team(), print("You chose to have more info about the volleyball team.\n"))
         
         # smoke test: valid inputs
     def test_correct_Q(self):
         # select some valid inputs, for which the output is known
-        self.assertEqual(my_input("Q"), "You chose to quit the program.")
-
+        self.assertEqual(my_input(), print("You chose to quit the program.\n"))
 
 
 # invalid inputs
     def test_wrong_values(self):
         # you should input wrong data
-        self.assertEqual(my_input("C"), None)
-        self.assertEqual(my_input(7), None)
+        self.assertEqual(info_player("C"), None)
+        self.assertEqual(info_team("C"), None)
+        self.assertEqual(my_quit("C"), None)
+        self.assertEqual(info_player(7), None)
+        self.assertEqual(info_team(7), None)
+        self.assertEqual(my_quit(7), None)
 
         # NOTE: the following test passing an empty list will fail!
         # self.assertEqual(<def_name>([]), None)
 
     # corner case: empty string
     def test_empty_string(self):
-        self.assertEqual(my_input(""), None)
+        self.assertEqual(info_player(""), None)
+        self.assertEqual(info_team(""), None)
+        self.assertEqual(my_quit(""), None)
 
 
-'''
-    # add parent folder to path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Functions import fun_name
-
-
-class TestFunctions(unittest.TestCase):
-
-    def setUp(self):
-        """Set the environment:
-           create the dataframe output for test.
-        """
-        test_row_0 = df[0]
-
-    # smoke test: valid inputs
-    def test_correct_values(self):
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(fun_name("SALA CHIARA"), test_row_0)
-
-    # invalid inputs
-    def test_wrong_values(self):
-        # you should input wrong data
-        self.assertEqual(fun_name("SALA CHIARA"), None)
-        self.assertEqual(fun_name(7), None)
-
-        # NOTE: the following test passing an empty list will fail!
-        # self.assertEqual(<def_name>([]), None)
-
-    # corner case: empty string
-    def test_empty_string(self):
-        self.assertEqual(fun_name(""), None)
-'''
 
 if __name__ == '__main__':
     
