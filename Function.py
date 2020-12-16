0c4ab20ff87a8fc6de13f1fcbfcd05e1c7c1709f
alessiazhou <alessiazhou99@example.com>
2020-12-16 16:06:01 +0100

    commented all the functions

--- Function.py
+++ Function.py
@@ -1,11 +1,11 @@
 from csv_read import DataBase as db
 
-
+#to be completed
 def check_player_all_stats(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         teams = db_manager.get_all_by_name(volley_player)
@@ -14,12 +14,12 @@ def check_player_all_stats(db_manager, volley_player):
         else:
             return f'Sorry, {volley_player} does not seem to be a known volley player.'
 
-
+#check player, if it is in the database, gives back her height
 def check_player_height(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         hg = db_manager.get_height_by_name(volley_player)
@@ -30,12 +30,12 @@ def check_player_height(db_manager, volley_player):
 
     return None
 
-
+#check player, if it is in the database, gives back her nationality
 def check_player_nationality(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         nat = db_manager.get_nationality_by_name(volley_player)
@@ -46,12 +46,12 @@ def check_player_nationality(db_manager, volley_player):
 
     return None
 
-
+#check player, if it is in the database, gives back her birthday
 def check_player_birth(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         bir = db_manager.get_birth_by_name(volley_player)
@@ -62,12 +62,12 @@ def check_player_birth(db_manager, volley_player):
 
     return None
 
-
+#check player, if it is in the database, gives back her role
 def check_player_role(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         rol = db_manager.get_role_by_name(volley_player)
@@ -78,12 +78,12 @@ def check_player_role(db_manager, volley_player):
 
     return None
 
-
+#check player, if it is in the database, gives back her team
 def check_player_team(db_manager, volley_player):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamnete.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         teams = db_manager.get_team_by_name(volley_player)
@@ -94,12 +94,12 @@ def check_player_team(db_manager, volley_player):
 
     return None
 
-
+#check team, if it is in the database, gives back its members
 def check_team(db_manager, team_name):
     """
     """
     if not isinstance(db_manager, db):
-        return f'il db non è stato caricato corettamente.'
+        return f'the database was not loaded correctly.'
 
     if db_manager:
         players = db_manager.get_player_by_team(team_name)
