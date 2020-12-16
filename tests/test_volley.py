deada4f0c6e392f09120579b844a1ed1a1ccfa29
Aiskorim <luisayang9@gmail.com>
2020-12-16 14:47:18 +0100

    test development branch

--- tests/test_volley.py
+++ tests/test_volley.py
@@ -11,7 +11,7 @@ from Main import my_input, info_player, info_team, my_quit
 class TestMain(unittest.TestCase):
 
 
-    # smoke test: valid inputs
+        # smoke test: valid inputs
     def test_correct_A(self):
         # select some valid inputs, for which the output is known
         self.assertEqual(info_player(), print("You chose to have more info about the volleyball player.\n"))
@@ -24,27 +24,37 @@ class TestMain(unittest.TestCase):
         # smoke test: valid inputs
     def test_correct_Q(self):
         # select some valid inputs, for which the output is known
-        self.assertEqual(my_input(), print("You chose to quit the program.\n"))
+        self.assertEqual(my_quit(), print("You chose to quit the program.\n"))
 
+        # false values
+    def test_false_values(self):
+        self.assertFalse(info_player(), None)
+        self.assertFalse(info_team(), None)
+        self.assertFalse(my_quit(), None)
+        self.assertFalse(info_player(), print("Your choice is not correct!!\n"))
+        self.assertFalse(info_team(), print("Your choice is not correct!!\n"))
+        self.assertFalse(my_quit(), print("Your choice is not correct!!\n"))
+        self.assertFalse(info_player(), 7)
+        self.assertFalse(info_team(), 7)
+        self.assertFalse(my_quit(), 7)
+        self.assertFalse(info_player(), "")
+        self.assertFalse(info_team(), "")
+        self.assertFalse(my_quit(), "")
 
-# invalid inputs
+        # invalid inputs
     def test_wrong_values(self):
         # you should input wrong data
-        self.assertEqual(info_player("C"), None)
-        self.assertEqual(info_team("C"), None)
-        self.assertEqual(my_quit("C"), None)
-        self.assertEqual(info_player(7), None)
-        self.assertEqual(info_team(7), None)
-        self.assertEqual(my_quit(7), None)
+        self.assertEqual(my_input("C"), "Your choice is not correct!!\n")
+        self.assertEqual(my_input(7), "Your choice is not correct!!\n")
+
+        # corner case: empty string
+    def test_empty_string(self):
+        self.assertEqual(my_input(""), print("Your choice is not correct!!\n"))
+
 
         # NOTE: the following test passing an empty list will fail!
         # self.assertEqual(<def_name>([]), None)
 
-    # corner case: empty string
-    def test_empty_string(self):
-        self.assertEqual(info_player(""), None)
-        self.assertEqual(info_team(""), None)
-        self.assertEqual(my_quit(""), None)
 
 
 
