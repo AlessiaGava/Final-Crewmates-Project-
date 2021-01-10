f6c8fdefd29cd731351406c449708bd73046b591
AlessiaGava <74045428+AlessiaGava@users.noreply.github.com>
2021-01-10 17:25:04 +0100

    modified the main file of the project

--- Main.py
+++ Main.py
@@ -1,50 +1,51 @@
-import Function
-import sys
 from os import system
 from time import sleep
+import argparse
+import extra_function
+import function
+import xls_read
 
-# A fuction to receive the choice of the user
-def my_input():   
-    print('What would you like to search?')
-    x = input('Type "A" if you want to know more info about a volleyball player.\nType "B" if you want to know '
-              'more info about a volleyball team.\nType "Q" if you want to quit the program.\nYour choice is: ')
-    return x
+# Definition of positional and optional arguments
+parser = argparse.ArgumentParser()
 
+parser.add_argument("xlsfile_fullpath", help="insert the full path of the xls file")
+parser.add_argument("-v", "--verbosity", action="count", help="increase output verbosity")
+# -v additional info for the user
+# -vv additional info for the user and the developer
+args = parser.parse_args()
+file_full_path = args.xlsfile_fullpath
 
-def info_player():
-    print("You chose to have more info about the volleyball player.\n")
+if args.verbosity is not None:
+    print("Verbosity turned on.")
+if args.verbosity == 1:
+    print("The xls file is in this path: " + file_full_path)
 
+# Database instance creation
+db = xls_read.DataBase(file_full_path)
+# Check if the database had been created
+if function.exist_db(db):
+    if args.verbosity == 2:
+        name = "SALA CHIARA"
+        print("TEST db: " + db.get_team_by_name(name))
+    # Cleaning the screen
+    system('cls')
+    # First question (what would you like to search)
+    choice = extra_function.input_request()
+    # According to the answer, a function is called
+    while True:
+        if choice == "A" or choice == "B" or choice == "Q":
+            if choice == "A":
+                print(extra_function.info_player(db))
+            if choice == "B":
+                print(extra_function.info_team(db))
+            if choice == "Q":
+                print(extra_function.quit_program())
+        else:
+            print("Your choice is not correct!\n")
+            # sleep for 2 seconds after printing output
+            sleep(2)
+            system('cls')
+            choice = extra_function.input_request()
 
-
-def info_team():
-    print("You chose to have more info about the volleyball team.\n")
-
-
-
-def my_quit():
-    print("You chose to quit the program.\n")
-
-
-# map the inputs to the function blocks
-options = {
-    "A": info_player,
-    "B": info_team,
-    "Q": my_quit,
-}
-
-while True:
-
-    choice = my_input()
-    if choice == "A" or choice == "B" or choice == "Q":
-        options[choice]()
-
-    else:
-        print("Your choice is not correct!!\n")
-        # sleep for 2 seconds after printing output 
-        sleep(2)
-        system('cls')
-        choice = my_input()
-
-    if choice == "Q":
-        break
-
+else:
+    print("There was an error in the creation of the database.")
