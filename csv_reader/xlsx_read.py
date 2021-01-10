d256e1e5d464868a24f15c2474a5954879925e15
Federico Montalesi <Federico.montalesi@gmail.com>
2021-01-10 15:31:44 +0100

    Doc string and rename

--- csv_reader/xlsx_read.py
+++ csv_reader/xlsx_read.py
@@ -1,20 +1,36 @@
 import sqlite3
 import pandas as pd
 
+'''possibili aggiunte sistema di gestione csv e xls caricamneto/salvataggio INTELIGENTE'''
+
 
 class DataBase():
     """
-        classe che carica e gestisce il dataset
-        varie funzioni
+    Class that import from an xlsx, instantiate and handle the database of player
+    contain several function to reacall info from database
+
+    :var self._table_name: The name of core table
+    :type self._table_name: str
+    :var self._df: pandas dataframe containing dataset of player
+    :type self._df: pandas.DataFrame
     """
 
     def __init__(self):
+        """
+        Constructor method
+        """
         self._table_name = "volley_player"
         self._df = pd.read_excel("Data/dataset_volley.xlsx")
         self._connect()
         self._create_team()
 
     def _connect(self):
+        """
+        Load dataset from dataframe
+
+        :var self.conn: loaded dataset in sqlite3
+        :type self.conn: sqlite3.Connection
+        """
         try:
             self.conn = sqlite3.connect(':memory:')
             self._df.to_sql(self._table_name, con=self.conn, if_exists='replace', index=True)
@@ -22,17 +38,41 @@ class DataBase():
             pass
 
     def _create_team(self):
+        """
+        Generate a table whit primary key team name
+
+        :var self.cursor: The cursor obj of dataset
+        :type self.cursor: sqlite3.cursor
+        """
         cursor = self.conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS volley_team(
                             TEAM TEXT PRIMARY KEY NOT NULL UNIQUE);''')
 
     def show_all(self):
+        """
+        Function that select all data of dateset and print them
+
+        :return: All data of players
+        :rtype: list
+        """
         return self.conn.execute("SELECT * FROM volley_player").fetchall()
 
     def show_col(self):
+        """
+        Extract from dataset all columns name
+
+        :return: Columns name
+        :rtype: list
+        """
         return self.conn.execute("PRAGMA table_info(volley_player);").fetchall()
 
     def get_name(self):
+        """
+        Extract from dataset all name of player
+
+        :return: Players name
+        :rtype: list
+        """
         lis = self.conn.execute('SELECT NAME FROM volley_player').fetchall()
         name = []
         for i in lis:
@@ -40,6 +80,13 @@ class DataBase():
         return name
 
     def get_all_by_name(self, player_name):
+        """
+        Extract from dataset all info of selected player
+
+        :param player_name: Full name of player searched
+        :return: Info of selected player
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT * FROM volley_player WHERE name=?', (player_name,))
@@ -54,6 +101,13 @@ class DataBase():
             return None
 
     def get_team_by_name(self, player_name):
+        """
+        Extract from dataset the name of the team to which the selected player belongs
+
+        :param player_name: Full name of player searched
+        :return: team name
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT TEAM FROM volley_player WHERE name=?', (player_name,))
@@ -68,6 +122,13 @@ class DataBase():
             return None
 
     def get_role_by_name(self, player_name):
+        """
+        Extract from dataset the role of the selected player
+
+        :param player_name: Full name of player searched
+        :return: player roles
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT ROLE FROM volley_player WHERE name=?', (player_name,))
@@ -82,6 +143,13 @@ class DataBase():
             return None
 
     def get_height_by_name(self, player_name):
+        """
+        Extract from dataset height of the selected player
+
+        :param player_name: Full name of player searched
+        :return: player heights
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT HEIGHT FROM volley_player WHERE name=?', (player_name,))
@@ -96,6 +164,13 @@ class DataBase():
             return None
 
     def get_nationality_by_name(self, player_name):
+        """
+        Extract from dataset nationality of the selected player
+
+        :param player_name: Full name of player searched
+        :return: player nationalities
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT NATIONALITY FROM volley_player WHERE name=?', (player_name,))
@@ -110,6 +185,13 @@ class DataBase():
             return None
 
     def get_birth_by_name(self, player_name):
+        """
+        Extract from dataset year of birth of the selected player
+
+        :param player_name: Full name of player searched
+        :return: player year of births
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             team = cursor.execute('SELECT BIRTH FROM volley_player WHERE name=?', (player_name,))
@@ -124,6 +206,13 @@ class DataBase():
             return None
 
     def get_player_by_team(self, team):
+        """
+        Extract form dataset names of all players belonging to a team
+
+        :param team: Name of a team
+        :return: Players names
+        :rtype: list
+        """
         cursor = self.conn.cursor()
         try:
             player = cursor.execute('SELECT NAME FROM volley_player WHERE TEAM=?', (team,))
@@ -141,6 +230,7 @@ class DataBase():
         self.conn.close()
 
 
+# testing thing
 if __name__ == "__main__":
     test = DataBase()
     name = "SALA CHIARA"
@@ -150,5 +240,5 @@ if __name__ == "__main__":
     print(test.get_role_by_name(name))
     print(test.get_height_by_name(name))
     print(test.get_nationality_by_name(name))
-    print(test.get_name())
+    print(test.get_all_by_name(name))
     test.close_con()
