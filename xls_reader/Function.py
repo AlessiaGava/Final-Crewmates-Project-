d2471ac317b6f50e6b7d761e262bf4058dfc7941
AlessiaGava <74045428+AlessiaGava@users.noreply.github.com>
2021-01-10 17:34:15 +0100

    improved Function.py

--- xls_reader/Function.py
+++ xls_reader/Function.py
@@ -1,136 +1,162 @@
-from csv_reader.csv_read import DataBase as db #questo è da sistemare
+from xls_read import DataBase
 
 
-def exist_player(db_manager, volley_player):
+def exist_db(my_db):
+    """
+    This function checks if the database has been created.
 
-    if volley_player in db_manager.get_name():
+    :param my_db: the database
+    :type my_db: Object
+    :return: True or False
+    :rtype: Boolean
+    """
+    if not isinstance(my_db, DataBase):
+        return False
+    return True
+
+
+def exist_player(my_db, volley_player):
+    """
+    This function checks if the name of the volleyball player exists in the database.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: True or False
+    :rtype: Boolean
+    """
+    if volley_player in my_db.get_name():
         return True
     return False
 
 
-def check_player_all_stats(db_manager, volley_player):
+def check_player_all_stats(my_db, volley_player):
     """
+    This function returns all the info about a volleyball player.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: all the info about the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        data_al = db_manager.get_all_by_name(volley_player)
-        if data_al:
-            return f'Team: {data_al[1]}, Nationality: {data_al[6]}, B_year: {data_al[4]}, Role: {data_al[3]}, Height: {data_al[5]}'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
+    data_al = my_db.get_all_by_name(volley_player)
+    if not data_al:
+        return None
+    return f'Team: {data_al[1]}, Nationality: {data_al[6]}, B_year: {data_al[4]}, Role: {data_al[3]}, ' \
+           f'Height: {data_al[5]}'
 
 
-def check_player_height(db_manager, volley_player):
+def check_player_height(my_db, volley_player):
     """
+    This function, given the database and the name of the volleyball player, returns her height using the method
+    get_height_by_name of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: the height of the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        hg = db_manager.get_height_by_name(volley_player)
-        if hg:
-            return f'{volley_player} is high {hg}.'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
-
-    return None
+    hg = my_db.get_height_by_name(volley_player)
+    return f'{volley_player} is {hg} cm tall.'
 
 
-def check_player_nationality(db_manager, volley_player):
+def check_player_nationality(my_db, volley_player):
     """
+    This function, given the database and the name of the volleyball player, returns her nationality using the method
+    get_nationality_by_name of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: the nationality of the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        nat = db_manager.get_nationality_by_name(volley_player)
-        if nat:
-            return f'The nationality of {volley_player} is {nat}.'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
-
-    return None
+    nat = my_db.get_nationality_by_name(volley_player)
+    return f'The nationality of {volley_player} is {nat}.'
 
 
-def check_player_birth(db_manager, volley_player):
+def check_player_birth(my_db, volley_player):
     """
+    This function, given the database and the name of the volleyball player, returns her year of birth using the method
+    get_birth_by_name of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return:the year of birth of the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        bir = db_manager.get_birth_by_name(volley_player)
-        if bir:
-            return f'The birth date of {volley_player} is {bir}.'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
-
-    return None
+    bir = my_db.get_birth_by_name(volley_player)
+    return f'The birth year of {volley_player} is {bir}.'
 
 
-def check_player_role(db_manager, volley_player):
+def check_player_role(my_db, volley_player):
     """
+    This function, given the database and the name of the volleyball player, returns her role using the method
+    get_role_by_name of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: the role of the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        rol = db_manager.get_role_by_name(volley_player)
-        if rol:
-            return f'The role of {volley_player} is {rol}.'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
-
-    return None
+    rol = my_db.get_role_by_name(volley_player)
+    return f'The role of {volley_player} is {rol}.'
 
 
-def check_player_team(db_manager, volley_player):
+def check_player_team(my_db, volley_player):
     """
+    This function, given the database and the name of the volleyball player, returns her team using the method
+    get_team_by_name of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param volley_player: name of the player
+    :type volley_player: String
+    :return: the team of the volleyball player
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        teams = db_manager.get_team_by_name(volley_player)
-        if teams:
-            return f'{volley_player} plays for {teams}.'
-        else:
-            return f'Sorry, {volley_player} does not seem to be a known volley player.'
-
-    return None
+    teams = my_db.get_team_by_name(volley_player)
+    return f'{volley_player} plays for {teams}.'
 
 
-def check_team(db_manager, team_name):
+def check_team(my_db, team_name):
     """
+    This function, given the database and the name of the volleyball team, returns its members using the method
+    get_player_by_team of the my_db object.
+
+    :param my_db: the database
+    :type my_db: Object
+    :param team_name: name of the team
+    :type team_name: String
+    :return: the members of the volleyball team
+    :rtype: String
     """
-    if not isinstance(db_manager, db):
-        return f'the database was not loaded correctly.'
-
-    if db_manager:
-        players = db_manager.get_player_by_team(team_name)
-
-        if players:
-            return f'The volley players of {team_name} are: {", ".join(x[0] for x in players)}.'
-        return f"Sorry, we don't know who are the players of {team_name}."
-
-    return None
+    players = my_db.get_player_by_team(team_name)
+    if players:
+        return f'The players of {team_name} are: {", ".join(x[0] for x in players)}.'
+    return f"Sorry, we don't know who are the players of {team_name}."
 
 
 if __name__ == "__main__":
     play = 'ANGELINA GIULIA'
-    team = "Banca Valsabbina Millenium Brescia"
-    data = db()
+    #play = 'NOBODY'
+    team = 'BANCA VALSABBINA MILLENIUM BRESCIA'
+    data = DataBase("C:\\Users\\Alessia\\PycharmProjects\\volleyproject\\Data\\dataset_volley.xlsx")
 
     print(check_team(data, team))
-    print(exist_player(data,play))
-
     print(check_player_all_stats(data, play))
     print(check_player_team(data, play))
     print(check_player_height(data, play))
     print(check_player_nationality(data, play))
     print(check_player_birth(data, play))
     print(check_player_role(data, play))
-
-# wrong names work, everything else doesn't
